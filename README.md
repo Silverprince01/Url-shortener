e36c28aa885bc59ce219e96452cb794985f12dbd
Badru Fuad Alabi <badrufuadalabi@gmail.com>
2022-02-27 10:52:46 +0100

    Added all files for pushing

---
+++ README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Shortly URL shortening API Challenge solution
+
+This is a solution to the [Shortly URL shortening API Challenge challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- Shorten any valid URL
+- See a list of their shortened links, even after refreshing the browser
+- Copy the shortened link to their clipboard in a single click
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/Silverprince01.com)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS custom properties
+- Flexbox
+- Javascript
+
+## Author
+
+- Name - Badru Fuad Alabi
+
---
+++ main.js
@@ -0,0 +1,59 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".ham");
+let longLink = document.getElementById("longLink");
+let shorten = document.querySelector(".shorten");
+const myShortUrl = "https://api.shrtco.de/v2/shorten?url=";
+const linkExtension = "/very/long/link.html"
+let result = document.querySelector(".result");
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
+
+shorten.addEventListener('click', async function getShortLink(){   
+    let newLink =   myShortUrl + longLink.value + linkExtension;
+    let response = await fetch(newLink);
+    let linkData = await response.json();
+    console.log(linkData);
+    const lonLink = document.createElement("p");
+    const shortLinkbox = document.createElement("div");
+    const newShortLink = document.createElement("div");
+    const shortLink = document.createElement("p");
+    const copybtn = document.createElement("input");
+    const line = document.createElement("hr");
+
+    copybtn.type = "button";
+    copybtn.name = "button";
+    copybtn.value = "Copy";
+
+    shortLinkbox.classList.add("box")
+    lonLink.classList.add("longLink");
+    newShortLink.classList.add("newShortLink");
+    shortLink.classList.add("ShortLink");
+    copybtn.classList.add("cop");
+    line.classList.add("hrline");
+    lonLink.textContent = linkData.result.original_link;
+    shortLink.textContent = linkData.result.short_link;
+if(longLink.value == ""){
+    alert("Enter a valid link")
+}else{
+shortLinkbox.appendChild(lonLink);
+    shortLinkbox.appendChild(line);
+    newShortLink.appendChild(shortLink);
+    newShortLink.appendChild(copybtn);
+    shortLinkbox.appendChild(newShortLink);
+    shortLinkbox.appendChild(newShortLink);
+    result.appendChild(shortLinkbox);
+
+    
+    copybtn.addEventListener('click', ()=>{
+        navigator.clipboard.writeText(shortLink.textContent);
+        copybtn.value = "Copied!";
+        copybtn.style.background = "hsl(257, 27%, 26%)"
+
+    })
+}   
+});
+
+
